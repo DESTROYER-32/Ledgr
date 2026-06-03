16a61589a6a39a31551db4c4cafb1b56c293ec6a
DESTROYER-32 <destroyer32@proton.me>
2026-06-03 08:49:50 +0000

    Bump compileSdk to 36 for flutter_plugin_android_lifecycle compatibility

--- budgetly/android/app/build.gradle.kts
+++ budgetly/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.budgetly.budgetly"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
