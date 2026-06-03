37383e0af578b35820e4e77fdfa23b9b09f91366
DESTROYER-32 <destroyer32@proton.me>
2026-06-03 10:38:32 +0000

    Fix compileSdk setter syntax and add local.properties generation in CI

--- budgetly/android/build.gradle.kts
+++ budgetly/android/build.gradle.kts
@@ -8,12 +8,12 @@ allprojects {
 subprojects {
     plugins.withType(com.android.build.gradle.LibraryPlugin::class.java) {
         extensions.configure(com.android.build.gradle.LibraryExtension::class.java) {
-            compileSdk = 36
+            setCompileSdk(36)
         }
     }
     plugins.withType(com.android.build.gradle.AppPlugin::class.java) {
         extensions.configure(com.android.build.gradle.AppExtension::class.java) {
-            compileSdk = 36
+            setCompileSdk(36)
         }
     }
 }
